e7a1a9e94812fbddf24d34cc74c6c85a218195da
abhishek <akey9009@gmail.com>
2025-06-18 15:19:16 +0530

    make changes in Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -130,11 +130,8 @@ export default function Navbar() {
             <img
               src={logoImg}
               alt="Tech Digital Logo"
-              className="h-16 w-auto"
+              className="h-10 w-auto"
             />
-            {/* <span className="text-2xl font-bold text-[#00a2ad]">
-              Tech Digital
-            </span> */}
           </Link>
 
           {/* Desktop Menu */}
